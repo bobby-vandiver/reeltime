543071449a9bd10333d2505c42507bc079dd09ef
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-06 15:28:06 -0500

    Add support for nested directories in local storage

--- grails-app/services/in/reeltime/storage/local/LocalFileSystemStorageService.groovy
+++ grails-app/services/in/reeltime/storage/local/LocalFileSystemStorageService.groovy
@@ -2,21 +2,37 @@ package in.reeltime.storage.local
 
 import in.reeltime.storage.StorageService
 
+import static java.io.File.separator
+
 class LocalFileSystemStorageService implements StorageService {
 
     @Override
     void store(InputStream inputStream, String basePath, String resourcePath) {
-        createDirectory(basePath)
-        createFile(basePath, resourcePath).withOutputStream { outputStream ->
-            outputStream << inputStream
-        }
+
+        def directory = getDirectory(basePath, resourcePath)
+        def filename = getFilename(resourcePath)
+
+        createDirectory(directory)
+        createFile(directory, filename).withOutputStream { outputStream -> outputStream << inputStream }
+    }
+
+    private String getDirectory(String parent, String child) {
+        def path = parent + separator + child
+        def lastSlash = path.lastIndexOf(separator)
+        return path.substring(0, lastSlash)
+    }
+
+    private String getFilename(String path) {
+        def lastSlash = path.lastIndexOf(separator)
+        def pathIsFilename = (lastSlash == -1)
+        pathIsFilename ? path : path.substring(lastSlash)
     }
 
     private void createDirectory(String path) {
         log.debug("Creating directory [$path]")
         def directory = new File(path)
         directory.deleteOnExit()
-        directory.mkdir()
+        directory.mkdirs()
     }
 
     private File createFile(String directory, String filename) {
--- test/unit/in/reeltime/storage/local/LocalFileSystemStorageServiceSpec.groovy
+++ test/unit/in/reeltime/storage/local/LocalFileSystemStorageServiceSpec.groovy
@@ -5,6 +5,8 @@ import in.reeltime.storage.StorageService
 import org.apache.commons.logging.Log
 import spock.lang.Specification
 
+import static java.io.File.separator
+
 @TestFor(LocalFileSystemStorageService)
 class LocalFileSystemStorageServiceSpec extends Specification {
 
@@ -15,7 +17,7 @@ class LocalFileSystemStorageServiceSpec extends Specification {
 
     void "basePath is the directory and resourcePath is the filename"() {
         given:
-        def directory = System.getProperty('java.io.tmpdir') + File.separator + UUID.randomUUID()
+        def directory = System.getProperty('java.io.tmpdir') + separator + UUID.randomUUID()
         def filename = 'test.txt'
 
         def contents = 'THIS IS A TEST'
@@ -28,7 +30,32 @@ class LocalFileSystemStorageServiceSpec extends Specification {
         assertFileContents(contents, directory, filename)
     }
 
+    void "resourcePath specifies a nested file"() {
+        given:
+        def directory = System.getProperty('java.io.tmpdir') + separator + UUID.randomUUID()
+        def filename = 'test.txt'
+
+        def relativePath = 'foo' + separator + 'bar' + separator
+        def filePath = relativePath + filename
+
+        def absolutePath = directory + separator + relativePath
+
+        and:
+        def contents = 'THIS IS A TEST'
+        def inputStream = new ByteArrayInputStream(contents.bytes)
+
+        when:
+        service.store(inputStream, directory, filePath)
+
+        then:
+        assertFileContents(contents, absolutePath, filename)
+    }
+
     private static void assertFileContents(String contents, String directory, String filename) {
+
+        println "***directory: $directory"
+        println "***filename: $filename"
+
         new File(directory, filename).withInputStream { assert it.bytes == contents.bytes }
     }
 
