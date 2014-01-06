df442bc0724e2bd3544512544f8b67af1fb28b8e
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-06 10:17:12 -0500

    Create TransferManager from S3 client

--- src/groovy/in/reeltime/storage/aws/TransferManagerFactory.groovy
+++ src/groovy/in/reeltime/storage/aws/TransferManagerFactory.groovy
@@ -1,10 +1,13 @@
 package in.reeltime.storage.aws
 
+import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.transfer.TransferManager
 
 class TransferManagerFactory {
 
     static TransferManager create() {
-        null
+        // TODO: Use encryption client
+        def s3 = new AmazonS3Client()
+        new TransferManager(s3)
     }
 }
