dedda39b1135c8ea6444b04c371fdaefd7271cac
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-05 22:10:09 -0500

    Add base path to StorageService interface

--- src/groovy/in/reeltime/storage/StorageService.groovy
+++ src/groovy/in/reeltime/storage/StorageService.groovy
@@ -2,5 +2,5 @@ package in.reeltime.storage
 
 interface StorageService {
 
-    void store(InputStream inputStream, String path)
+    void store(InputStream inputStream, String basePath, String resourcePath)
 }
