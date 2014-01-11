f4a2a8877873229d97581b1f38c90199652fe6be
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-11 17:46:19 -0500

    Cleaning up ElasticTranscoderService tests

--- test/unit/in/reeltime/transcoder/aws/ElasticTranscoderServiceSpec.groovy
+++ test/unit/in/reeltime/transcoder/aws/ElasticTranscoderServiceSpec.groovy
@@ -2,7 +2,6 @@ package in.reeltime.transcoder.aws
 
 import com.amazonaws.services.elastictranscoder.AmazonElasticTranscoder
 import com.amazonaws.services.elastictranscoder.model.CreateJobOutput
-import com.amazonaws.services.elastictranscoder.model.CreateJobPlaylist
 import com.amazonaws.services.elastictranscoder.model.CreateJobRequest
 import com.amazonaws.services.elastictranscoder.model.CreateJobResult
 import com.amazonaws.services.elastictranscoder.model.Job
@@ -21,6 +20,36 @@ class ElasticTranscoderServiceSpec extends Specification {
 
     private static final UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
 
+    private Pipeline pipeline
+    private AmazonElasticTranscoder mockElasticTranscoder
+
+    void setup() {
+        pipeline = new Pipeline(name: 'testPipeline', id: '132afc')
+
+        mockElasticTranscoder = Mock(AmazonElasticTranscoder) {
+            listPipelines() >> Stub(ListPipelinesResult) {
+                getPipelines() >> [pipeline]
+            }
+        }
+
+        grailsApplication.config.transcoder.pipeline = pipeline.name
+
+        grailsApplication.config.transcoder.input.aspectRatio = 'auto'
+        grailsApplication.config.transcoder.input.frameRate = 'auto'
+        grailsApplication.config.transcoder.input.resolution = 'auto'
+        grailsApplication.config.transcoder.input.interlaced = 'auto'
+        grailsApplication.config.transcoder.input.container = 'auto'
+
+        grailsApplication.config.transcoder.output.segmentDuration = '10'
+        grailsApplication.config.transcoder.output.format = 'HLSv3'
+        grailsApplication.config.transcoder.output.presets = ['400k': '1351620000001-200050']
+
+        service.grailsApplication = grailsApplication
+
+        service.transcoderJobService = Mock(TranscoderJobService)
+        service.awsService = Mock(AwsService)
+    }
+
     void "must be an instance of TranscoderService"() {
         expect:
         service instanceof TranscoderService
@@ -28,32 +57,6 @@ class ElasticTranscoderServiceSpec extends Specification {
 
     void "submit video to configured pipeline"() {
         given:
-        def mockEts = Mock(AmazonElasticTranscoder)
-
-        and:
-        service.awsService = Mock(AwsService)
-        service.transcoderJobService = Mock(TranscoderJobService)
-
-        and:
-        def pipeline = new Pipeline(name: 'foo', id: '132afc')
-        grailsApplication.config.transcoder.pipeline = 'foo'
-
-        and:
-        grailsApplication.config.transcoder.input.aspectRatio = 'auto'
-        grailsApplication.config.transcoder.input.frameRate = 'auto'
-        grailsApplication.config.transcoder.input.resolution = 'auto'
-        grailsApplication.config.transcoder.input.interlaced = 'auto'
-        grailsApplication.config.transcoder.input.container = 'auto'
-
-        and:
-        grailsApplication.config.transcoder.output.segmentDuration = '10'
-        grailsApplication.config.transcoder.output.format = 'HLSv3'
-        grailsApplication.config.transcoder.output.presets = ['400k': '1351620000001-200050']
-
-        and:
-        service.grailsApplication = grailsApplication
-
-        and:
         def video = new Video(masterPath: 'bar')
         def jobId = '123413212351'
 
@@ -61,31 +64,13 @@ class ElasticTranscoderServiceSpec extends Specification {
         service.transcode(video)
 
         then:
-        1 * service.awsService.createClient(AmazonElasticTranscoder) >> mockEts
+        1 * service.awsService.createClient(AmazonElasticTranscoder) >> mockElasticTranscoder
 
         and:
-        1 * mockEts.listPipelines() >> Stub(ListPipelinesResult) {
-            getPipelines() >> [pipeline]
-        }
+        1 * mockElasticTranscoder.createJob(_) >> { CreateJobRequest request ->
 
-        and:
-        1 * mockEts.createJob(_) >> { CreateJobRequest request ->
-
-            assert request.pipelineId == pipeline.id
-
-            def outputKeyPrefix = request.outputKeyPrefix
-            assert outputKeyPrefix.endsWith('/')
-            assert outputKeyPrefix[0..-2].matches(UUID_REGEX)
-
-            def outputs = request.outputs
-            assert outputs.size() == 1
-            validateJobOutput(request.outputs[0])
-
-            def playlists = request.playlists
-            assert playlists.size() == 1
-            validatePlaylist(playlists[0])
-
-            validateJobInput(request.input, video.masterPath)
+            def validator = new CreateJobRequestValidator(request)
+            validator.validate([pipelineId: pipeline.id, mediaPlaylistCount: 1, inputKey: video.masterPath])
 
             def job = Stub(Job) { getId() >> jobId }
             return Stub(CreateJobResult) { getJob() >> job }
@@ -95,26 +80,61 @@ class ElasticTranscoderServiceSpec extends Specification {
         1 * service.transcoderJobService.createJob(video, jobId)
     }
 
-    private void validatePlaylist(CreateJobPlaylist playlist) {
-        assert playlist.format == 'HLSv3'
-        assert playlist.name.matches(UUID_REGEX)
+    private static class CreateJobRequestValidator {
 
-        assert playlist.outputKeys.size() == 1
-        assert playlist.outputKeys[0].matches(UUID_REGEX)
-    }
+        private final CreateJobRequest request
 
-    private void validateJobOutput(CreateJobOutput output) {
-        assert output.key.matches(UUID_REGEX)
-        assert output.presetId == '1351620000001-200050'
-        assert output.segmentDuration == '10'
-    }
+        CreateJobRequestValidator(CreateJobRequest request) {
+            this.request = request
+        }
 
-    private void validateJobInput(JobInput input, String expectedKey) {
-        assert input.key == expectedKey
-        assert input.aspectRatio == 'auto'
-        assert input.frameRate == 'auto'
-        assert input.resolution == 'auto'
-        assert input.interlaced == 'auto'
-        assert input.container == 'auto'
+        void validate(Map expectations) {
+            pipeline(expectations.pipelineId)
+            outputKeyPrefix()
+            variantPlaylist()
+            mediaPlaylists(expectations.mediaPlaylistCount)
+            jobInput(expectations.inputKey)
+        }
+
+        private void pipeline(pipelineId) {
+            assert request.pipelineId == pipelineId
+        }
+
+        private void outputKeyPrefix() {
+            def outputKeyPrefix = request.outputKeyPrefix
+            assert outputKeyPrefix.endsWith('/')
+            assert outputKeyPrefix[0..-2].matches(UUID_REGEX)
+        }
+
+        private void variantPlaylist() {
+            assert request.playlists.size() == 1
+
+            def variantPlaylist = request.playlists[0]
+            assert variantPlaylist.format == 'HLSv3'
+            assert variantPlaylist.name.matches(UUID_REGEX)
+        }
+
+        private void mediaPlaylists(int mediaPlaylistCount) {
+
+            def outputKeys = request.playlists[0].outputKeys
+            assert outputKeys.size() == mediaPlaylistCount
+            outputKeys.each { assert it.matches(UUID_REGEX) }
+
+            request.outputs.each { output ->
+                assert outputKeys.contains(output.key)
+                assert output.presetId == '1351620000001-200050'
+                assert output.segmentDuration == '10'
+            }
+        }
+
+        private void jobInput(String expectedKey) {
+            def input = request.input
+            assert input.key == expectedKey
+            assert input.aspectRatio == 'auto'
+            assert input.frameRate == 'auto'
+            assert input.resolution == 'auto'
+            assert input.interlaced == 'auto'
+            assert input.container == 'auto'
+        }
     }
 }
