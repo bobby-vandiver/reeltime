4d0f6d9c35483ed42b69ca9be359f0afaac4b335
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-12 01:51:48 -0500

    Implement unique path generation

--- grails-app/services/in/reeltime/storage/StorageNameService.groovy
+++ grails-app/services/in/reeltime/storage/StorageNameService.groovy
@@ -2,11 +2,28 @@ package in.reeltime.storage
 
 class StorageNameService {
 
-    String getUniqueInputPath() {
+    def storageService
+    def grailsApplication
 
+    String getUniqueInputPath() {
+        def inputBase = grailsApplication.config.storage.input
+        generateRandomUniquePath(inputBase)
     }
 
     String getUniqueOutputPath() {
+        def outputBase = grailsApplication.config.storage.output
+        generateRandomUniquePath(outputBase)
+    }
 
+    private String generateRandomUniquePath(base) {
+        def path = randomUUIDString()
+        while (!storageService.available(base, path)) {
+            path = randomUUIDString()
+        }
+        return path
+    }
+
+    private static String randomUUIDString() {
+        UUID.randomUUID() as String
     }
 }
---
+++ test/unit/in/reeltime/storage/StorageNameServiceSpec.groovy
@@ -0,0 +1,59 @@
+package in.reeltime.storage
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@TestFor(StorageNameService)
+class StorageNameServiceSpec extends Specification {
+
+    private static final UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
+
+    @Unroll
+    void "unique file generated the first time for [#configName]"() {
+        given:
+        grailsApplication.config.storage[configName] = base
+
+        and:
+        service.grailsApplication = grailsApplication
+        service.storageService = Mock(StorageService)
+
+        when:
+        def name = service."$methodName"()
+
+        then:
+        name.matches(UUID_REGEX)
+
+        and:
+        1 * service.storageService.available(base, _) >> true
+
+        where:
+        configName      |   base        |   methodName
+        'input'         |   'inbox'     |   'getUniqueInputPath'
+        'output'        |   'outbox'    |   'getUniqueOutputPath'
+    }
+
+    @Unroll
+    void "unique file generated after the second time for [#configName]"() {
+        given:
+        grailsApplication.config.storage[configName] = base
+
+        and:
+        service.grailsApplication = grailsApplication
+        service.storageService = Mock(StorageService)
+
+        when:
+        def name = service."$methodName"()
+
+        then:
+        name.matches(UUID_REGEX)
+
+        and:
+        2 * service.storageService.available(base, _) >>> [false, true]
+
+        where:
+        configName      |   base        |   methodName
+        'input'         |   'inbox'     |   'getUniqueInputPath'
+        'output'        |   'outbox'    |   'getUniqueOutputPath'
+    }
+}
