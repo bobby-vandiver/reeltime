9e3178c87d441db1a86ce0cca5f12a6e802509aa
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-12 18:26:48 -0500

    Wire environment specific TranscoderService bean

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -4,16 +4,13 @@ beans = {
 
     Environment.executeForCurrentEnvironment {
 
-        test {
-            springConfig.addAlias 'storageService', 'localFileSystemStorage'
-        }
-
-        development {
-            springConfig.addAlias 'storageService', 'localFileSystemStorage'
-        }
+        // Use local file system and ffmpeg unless in production on AWS
+        springConfig.addAlias 'storageService', 'localFileSystemStorage'
+        springConfig.addAlias 'transcoderService', 'ffmpegTranscoderService'
 
         production {
             springConfig.addAlias 'storageService', 's3StorageService'
+            springConfig.addAlias 'transcoderService', 'elasticTranscoderService'
         }
     }
 }
