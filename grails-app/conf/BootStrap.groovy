b0949d93cb54d4b136c9ffa61f6ef50f8cffba7e
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-12 19:53:31 -0500

    Check for dummy user before creating

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,16 +4,18 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        User user = new User(
-                username: 'bob',
-                password: 'pass',
-                enabled: true,
-                accountExpired: false,
-                accountLocked: false,
-                passwordExpired: false
-        )
-        user.save(flush: true)
-        log.info("Added user [${user.username}] with id [${user.id}]")
+        if(!User.findByUsername('bob')) {
+            User user = new User(
+                    username: 'bob',
+                    password: 'pass',
+                    enabled: true,
+                    accountExpired: false,
+                    accountLocked: false,
+                    passwordExpired: false
+            )
+            user.save(flush: true)
+            log.info("Added user [${user.username}] with id [${user.id}]")
+        }
     }
 
     def destroy = {
