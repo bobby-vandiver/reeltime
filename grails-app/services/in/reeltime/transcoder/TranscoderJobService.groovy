9a3de7bae5a2c62c1564a15a8bff9ee4ea9a7c62
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-12 20:07:17 -0500

    Add logging to TranscoderJobService

--- grails-app/services/in/reeltime/transcoder/TranscoderJobService.groovy
+++ grails-app/services/in/reeltime/transcoder/TranscoderJobService.groovy
@@ -5,7 +5,7 @@ import in.reeltime.video.Video
 class TranscoderJobService {
 
     def createJob(Video video, String jobId) {
-
+        log.info("Creating TranscoderJob with jobId [$jobId] for video [${video.id}")
     }
 
     def complete(String jobId) {
