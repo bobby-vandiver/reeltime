303eaaf00fc1bec8bc18e34e2a390a03e7bf1565
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-14 19:40:57 -0500

    Load and parse playlists

--- grails-app/services/in/reeltime/video/playlist/PlaylistParsingService.groovy
+++ grails-app/services/in/reeltime/video/playlist/PlaylistParsingService.groovy
@@ -1,12 +1,22 @@
 package in.reeltime.video.playlist
 
+import in.reeltime.hls.playlist.parser.VariantPlaylistParser
+import in.reeltime.hls.playlist.parser.MediaPlaylistParser
+
 class PlaylistParsingService {
 
-    def parseVariantPlaylist(String path) {
+    def outputStorageService
 
+    def parseVariantPlaylist(String path) {
+        parse(path) { reader -> VariantPlaylistParser.parse(reader) }
     }
 
     def parseMediaPlaylist(String path) {
+        parse(path) { reader -> MediaPlaylistParser.parse(reader) }
+    }
 
+    private def parse(String path, Closure parser) {
+        def playlistStream = outputStorageService.load(path) as InputStream
+        playlistStream.withReader { reader -> parser(reader) }
     }
 }
---
+++ test/unit/in/reeltime/video/playlist/PlaylistParsingServiceSpec.groovy
@@ -0,0 +1,146 @@
+package in.reeltime.video.playlist
+
+import grails.test.mixin.TestFor
+import in.reeltime.hls.playlist.MediaPlaylist
+import in.reeltime.hls.playlist.VariantPlaylist
+import spock.lang.Specification
+import in.reeltime.storage.OutputStorageService
+
+@TestFor(PlaylistParsingService)
+class PlaylistParsingServiceSpec extends Specification {
+
+    void "load variant playlist from storage and parse it"() {
+        given:
+        def text = '''#EXTM3U
+                     |#EXT-X-STREAM-INF:PROGRAM-ID=1,RESOLUTION=400x226,CODECS="avc1.42001e,mp4a.40.2",BANDWIDTH=455000
+                     |hls-bats-400k.m3u8
+                     |#EXT-X-STREAM-INF:PROGRAM-ID=1,RESOLUTION=480x270,CODECS="avc1.42001e,mp4a.40.2",BANDWIDTH=663000
+                     |hls-bats-600k.m3u8'''.stripMargin()
+
+        def playlistStream = new ByteArrayInputStream(text.bytes)
+
+        and:
+        def path = 'holy-streaming-video-batman/hls-bats-master.m3u8'
+        service.outputStorageService = Mock(OutputStorageService)
+
+        when:
+        def variantPlaylist = service.parseVariantPlaylist(path) as VariantPlaylist
+
+        then:
+        1 * service.outputStorageService.load(path) >> playlistStream
+
+        and:
+        variantPlaylist.streams.size() == 2
+
+        and:
+        def stream1 = variantPlaylist.streams[0]
+        def stream2 = variantPlaylist.streams[1]
+
+        and:
+        stream1.uri == 'hls-bats-400k.m3u8'
+        stream1.programId == 1
+        stream1.resolution == '400x226'
+        stream1.codecs == 'avc1.42001e,mp4a.40.2'
+        stream1.bandwidth == 455000
+
+        and:
+        stream2.uri == 'hls-bats-600k.m3u8'
+        stream2.programId == 1
+        stream2.resolution == '480x270'
+        stream2.codecs == 'avc1.42001e,mp4a.40.2'
+        stream2.bandwidth == 663000
+    }
+
+    void "load media playlist from storage and parse it"() {
+        given:
+        def text = '''#EXTM3U
+                     |#EXT-X-VERSION:3
+                     |#EXT-X-MEDIA-SEQUENCE:0
+                     |#EXT-X-ALLOW-CACHE:YES
+                     |#EXT-X-TARGETDURATION:12
+                     |#EXTINF:11.308056,
+                     |hls-bats-400k00000.ts
+                     |#EXTINF:11.262044,
+                     |hls-bats-400k00001.ts
+                     |#EXTINF:7.524100,
+                     |hls-bats-400k00002.ts
+                     |#EXTINF:11.278267,
+                     |hls-bats-400k00003.ts
+                     |#EXTINF:11.278689,
+                     |hls-bats-400k00004.ts
+                     |#EXTINF:7.517500,
+                     |hls-bats-400k00005.ts
+                     |#EXTINF:11.271656,
+                     |hls-bats-400k00006.ts
+                     |#EXTINF:11.272111,
+                     |hls-bats-400k00007.ts
+                     |#EXTINF:7.510911,
+                     |hls-bats-400k00008.ts
+                     |#EXTINF:0.792889,
+                     |hls-bats-400k00009.ts
+                     |#EXT-X-ENDLIST'''.stripMargin()
+
+        def playlistStream = new ByteArrayInputStream(text.bytes)
+
+        and:
+        def path = 'holy-streaming-video-batman/hls-bats-400k.m3u8'
+        service.outputStorageService = Mock(OutputStorageService)
+
+        when:
+        def mediaPlaylist = service.parseMediaPlaylist(path) as MediaPlaylist
+
+        then:
+        1 * service.outputStorageService.load(path) >> playlistStream
+
+        and:
+        mediaPlaylist.version == 3
+        mediaPlaylist.mediaSequence == 0
+        mediaPlaylist.targetDuration == 12
+        mediaPlaylist.allowCache
+
+        and:
+        def segments = mediaPlaylist.segments
+        segments.size() == 10
+
+        and:
+        segments[0].uri == 'hls-bats-400k00000.ts'
+        segments[0].duration == '11.308056'
+
+        and:
+        segments[1].uri == 'hls-bats-400k00001.ts'
+        segments[1].duration == '11.262044'
+
+        and:
+        segments[2].uri == 'hls-bats-400k00002.ts'
+        segments[2].duration == '7.524100'
+
+        and:
+        segments[3].uri == 'hls-bats-400k00003.ts'
+        segments[3].duration == '11.278267'
+
+        and:
+        segments[4].uri == 'hls-bats-400k00004.ts'
+        segments[4].duration == '11.278689'
+
+        and:
+        segments[5].uri == 'hls-bats-400k00005.ts'
+        segments[5].duration == '7.517500'
+
+        and:
+        segments[6].uri == 'hls-bats-400k00006.ts'
+        segments[6].duration == '11.271656'
+
+        and:
+        segments[7].uri == 'hls-bats-400k00007.ts'
+        segments[7].duration == '11.272111'
+
+        and:
+        segments[8].uri == 'hls-bats-400k00008.ts'
+        segments[8].duration == '7.510911'
+
+        and:
+        segments[9].uri == 'hls-bats-400k00009.ts'
+        segments[9].duration == '0.792889'
+    }
+
+}
