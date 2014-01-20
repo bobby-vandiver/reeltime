aab7c81f58ba8ddaa0d72726ebc72344c5122734
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-01-20 17:34:59 -0500

    Add VariantPlaylistComposer

---
+++ src/groovy/in/reeltime/hls/playlist/composer/VariantPlaylistComposer.groovy
@@ -0,0 +1,26 @@
+package in.reeltime.hls.playlist.composer
+
+import in.reeltime.hls.playlist.StreamAttributes
+
+class VariantPlaylistComposer {
+
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator")
+
+    static void compose(Collection<StreamAttributes> streams, Writer writer) {
+
+        def builder = new StringBuilder().append("#EXTM3U")
+
+        streams.each { stream ->
+            builder.append(LINE_SEPARATOR)
+                   .append("#EXT-X-STREAM-INF:")
+                   .append("PROGRAM-ID=${stream.programId},")
+                   .append("RESOLUTION=${stream.resolution},")
+                   .append("CODECS=\"${stream.codecs}\",")
+                   .append("BANDWIDTH=${stream.bandwidth}")
+                   .append(LINE_SEPARATOR)
+                   .append(stream.uri)
+        }
+
+        writer << builder.toString()
+    }
+}
---
+++ test/unit/in/reeltime/hls/playlist/composer/VariantPlaylistComposerSpec.groovy
@@ -0,0 +1,70 @@
+package in.reeltime.hls.playlist.composer
+
+import in.reeltime.hls.playlist.StreamAttributes
+import spock.lang.Specification
+
+class VariantPlaylistComposerSpec extends Specification {
+
+    void "no streams"() {
+        given:
+        def writer = new StringWriter()
+
+        when:
+        VariantPlaylistComposer.compose([], writer)
+
+        then:
+        writer.toString() == '#EXTM3U'
+    }
+
+    void "single stream variant"() {
+        given:
+        def stream = new StreamAttributes(
+                uri: 'hls-bats-400k.m3u8',
+                bandwidth: 455000,
+                programId: 1,
+                codecs: 'avc1.42001e,mp4a.40.2',
+                resolution:'400x226'
+        )
+
+        def writer = new StringWriter()
+
+        when:
+        VariantPlaylistComposer.compose([stream], writer)
+
+        then:
+        writer.toString() == '''#EXTM3U
+                               |#EXT-X-STREAM-INF:PROGRAM-ID=1,RESOLUTION=400x226,CODECS="avc1.42001e,mp4a.40.2",BANDWIDTH=455000
+                               |hls-bats-400k.m3u8'''.stripMargin()
+    }
+
+    void "multiple stream variants"() {
+        given:
+        def stream1 = new StreamAttributes(
+                uri: 'hls-bats-400k.m3u8',
+                bandwidth: 455000,
+                programId: 1,
+                codecs: 'avc1.42001e,mp4a.40.2',
+                resolution:'400x226'
+        )
+
+        def stream2 = new StreamAttributes(
+                uri: 'hls-bats-600k.m3u8',
+                bandwidth: 663000,
+                programId: 1,
+                codecs: 'avc1.42001e,mp4a.40.2',
+                resolution:'480x270'
+        )
+
+        def writer = new StringWriter()
+
+        when:
+        VariantPlaylistComposer.compose([stream1, stream2], writer)
+
+        then:
+        writer.toString() == '''#EXTM3U
+                               |#EXT-X-STREAM-INF:PROGRAM-ID=1,RESOLUTION=400x226,CODECS="avc1.42001e,mp4a.40.2",BANDWIDTH=455000
+                               |hls-bats-400k.m3u8
+                               |#EXT-X-STREAM-INF:PROGRAM-ID=1,RESOLUTION=480x270,CODECS="avc1.42001e,mp4a.40.2",BANDWIDTH=663000
+                               |hls-bats-600k.m3u8'''.stripMargin()
+    }
+}
