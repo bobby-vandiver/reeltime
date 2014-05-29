93ffead3645800b8c9e626995fcbb300348af047
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-05-28 21:12:29 -0400

    Pull mock initialization into setup

--- test/unit/in/reeltime/notification/NotificationControllerSpec.groovy
+++ test/unit/in/reeltime/notification/NotificationControllerSpec.groovy
@@ -14,15 +14,29 @@ import in.reeltime.playlist.PlaylistService
 @Mock([TranscoderJob])
 class NotificationControllerSpec extends Specification {
 
+    NotificationService notificationService
+    TranscoderJobService transcoderJobService
+    PlaylistService playlistService
+
+    void setup() {
+        notificationService = Mock(NotificationService)
+        transcoderJobService = Mock(TranscoderJobService)
+        playlistService = Mock(PlaylistService)
+
+        controller.notificationService = notificationService
+        controller.transcoderJobService = transcoderJobService
+        controller.playlistService = playlistService
+    }
+
     @Unroll
     void "message is authentic [#authentic]"() {
         given:
-        controller.notificationService = Mock(NotificationService)
-
         def signingCertUrl = 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-e372f8ca30337fdb084e8ac449342c77.pem'
+        def token = '2336412f37fb687f5d51e6e241d164b051479845a45fd1e10f1287fbc675dba8bb330f79de4343d9bc6e25954e0b9b47c04d6f9d46d7f52460b8f253675f7909d0d801fa1fb7af7aac2400e9491e815b2b506921d04a2a918d70a75f5768b654b6ad6da9bce8c4c98eb6f16857123e51'
+        def topicArn = 'arn:aws:sns:us-east-1:166209233708:ets-listener'
 
-        def message = /{"SigningCertURL": "${signingCertUrl}"}/
-        def parsedMessage = [SigningCertURL: signingCertUrl]
+        def message = /{"SigningCertURL": "${signingCertUrl}", "Token": "${token}", "TopicArn": "${topicArn}"}/
+        def parsedMessage = [SigningCertURL: signingCertUrl, Token: token, TopicArn: topicArn]
 
         and:
         request.content = message.bytes
@@ -38,7 +52,7 @@ class NotificationControllerSpec extends Specification {
         (response.status == 400) == statusIs400
 
         and:
-        1 * controller.notificationService.verifyMessage(parsedMessage) >> authentic
+        1 * notificationService.verifyMessage(parsedMessage) >> authentic
 
         where:
         authentic   |   resultIsNull    |   resultIsFalse   |   statusIs400
@@ -81,9 +95,6 @@ class NotificationControllerSpec extends Specification {
     @Unroll
     void "return 400 if SubscriptionConfirmation message is empty for action [#action]"() {
         given:
-        controller.notificationService = Mock(NotificationService)
-
-        and:
         request.addHeader('x-amz-sns-message-type', 'SubscriptionConfirmation')
         request.content = '{}'.bytes
 
@@ -94,7 +105,7 @@ class NotificationControllerSpec extends Specification {
         response.status == 400
 
         and:
-        0 * controller.notificationService.confirmSubscription(_)
+        0 * notificationService.confirmSubscription(*_)
 
         where:
         action << ['completed', 'progressing', 'warning', 'error']
@@ -103,9 +114,6 @@ class NotificationControllerSpec extends Specification {
     @Unroll
     void "return 200 after confirming subscription for action [#action]"() {
         given:
-        controller.notificationService = Mock(NotificationService)
-
-        and:
         def token = '2336412f37fb687f5d51e6e241d164b051479845a45fd1e10f1287fbc675dba8bb330f79de4343d9bc6e25954e0b9b47c04d6f9d46d7f52460b8f253675f7909d0d801fa1fb7af7aac2400e9491e815b2b506921d04a2a918d70a75f5768b654b6ad6da9bce8c4c98eb6f16857123e51'
         def topicArn = 'arn:aws:sns:us-east-1:166209233708:ets-listener'
         def message = /{"Token": "${token}", "TopicArn": "${topicArn}"}/
@@ -121,7 +129,7 @@ class NotificationControllerSpec extends Specification {
         response.status == 200
 
         and:
-        1 * controller.notificationService.confirmSubscription(topicArn, token)
+        1 * notificationService.confirmSubscription(topicArn, token)
 
         where:
         action << ['completed', 'progressing', 'warning', 'error']
@@ -181,10 +189,6 @@ class NotificationControllerSpec extends Specification {
 
     void "load the elastic transcoder job and delegate to services to complete transcoding process"() {
         given:
-        controller.transcoderJobService = Mock(TranscoderJobService)
-        controller.playlistService = Mock(PlaylistService)
-
-        and:
         def message = '''{
                         |    "Message": "{\\n  \\"state\\" : \\"COMPLETED\\",\\n  \\"version\\" : \\"2012-09-25\\",\\n  \\"jobId\\" : \\"1388444889472-t01s28\\",\\n  \\"pipelineId\\" : \\"1388441748515-gvt196\\",\\n  \\"input\\" : {\\n    \\"key\\" : \\"small.mp4\\",\\n    \\"frameRate\\" : \\"auto\\",\\n    \\"resolution\\" : \\"auto\\",\\n    \\"aspectRatio\\" : \\"auto\\",\\n    \\"interlaced\\" : \\"auto\\",\\n    \\"container\\" : \\"auto\\"\\n  },\\n  \\"outputKeyPrefix\\" : \\"hls-small/\\",\\n  \\"outputs\\" : [ {\\n    \\"id\\" : \\"1\\",\\n    \\"presetId\\" : \\"1351620000001-200050\\",\\n    \\"key\\" : \\"hls-small-400k\\",\\n    \\"thumbnailPattern\\" : \\"\\",\\n    \\"rotate\\" : \\"auto\\",\\n    \\"segmentDuration\\" : 10.0,\\n    \\"status\\" : \\"Complete\\",\\n    \\"duration\\" : 6,\\n    \\"width\\" : 400,\\n    \\"height\\" : 228\\n  } ],\\n  \\"playlists\\" : [ {\\n    \\"name\\" : \\"hls-small-master\\",\\n    \\"format\\" : \\"HLSv3\\",\\n    \\"outputKeys\\" : [ \\"hls-small-400k\\" ],\\n    \\"status\\" : \\"Complete\\"\\n  } ]\\n}",
                         |    "Subject": "Amazon Elastic Transcoder has finished transcoding job 1388444889472-t01s28.",
@@ -203,8 +207,8 @@ class NotificationControllerSpec extends Specification {
         controller.completed()
 
         then:
-        1 * controller.transcoderJobService.complete(transcoderJob)
-        1 * controller.playlistService.addPlaylists(video, 'hls-small/', 'hls-small-master')
+        1 * transcoderJobService.complete(transcoderJob)
+        1 * playlistService.addPlaylists(video, 'hls-small/', 'hls-small-master')
 
         and:
         response.status == 200
