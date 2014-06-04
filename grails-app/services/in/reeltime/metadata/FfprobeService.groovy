5e28274de5cdad3eaae261f87eafd8a84b516c8e
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-06-04 18:24:15 -0400

    Probe video files with ffprobe

--- grails-app/services/in/reeltime/metadata/FfprobeService.groovy
+++ grails-app/services/in/reeltime/metadata/FfprobeService.groovy
@@ -1,5 +1,35 @@
 package in.reeltime.metadata
 
+import groovy.json.JsonSlurper
+
 class FfprobeService {
 
+    def grailsApplication
+
+    Map probeVideo(File video) {
+        log.debug("Entering ${this.class.simpleName} probeVideo for video [${video.absolutePath}]")
+
+        def ffprobe = grailsApplication.config.reeltime.metadata.ffprobe
+        ensurePathToFfprobeIsDefined(ffprobe)
+
+        def command = "${ffprobe} -v quiet -print_format json -show_streams ${video.absolutePath}"
+        def process = command.execute()
+        process.waitFor()
+
+        log.info("Finished probing video [${video.path}]")
+
+        def jsonText = process.in.text
+        log.debug("jsonText from ffprobe: $jsonText")
+
+        new JsonSlurper().parseText(jsonText)
+    }
+
+    private static void ensurePathToFfprobeIsDefined(String ffprobe) {
+        if(!ffprobe || pathIsInvalid(ffprobe))
+            throw new IllegalStateException('ffprobe could not be found')
+    }
+
+    private static boolean pathIsInvalid(String ffprobe) {
+        !new File(ffprobe).exists()
+    }
 }
--- test/unit/in/reeltime/metadata/FfprobeServiceSpec.groovy
+++ test/unit/in/reeltime/metadata/FfprobeServiceSpec.groovy
@@ -2,7 +2,29 @@ package in.reeltime.metadata
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@TestFor(FfprobeService)
 class FfprobeServiceSpec extends Specification {
 
+    @Unroll
+    void "throw if ffprobe cannot be found at path [#path]"() {
+        given:
+        grailsApplication.config.reeltime.metadata.ffprobe = path
+
+        and:
+        def file = File.createTempFile('ffprobe-not-found-test', '.mp4')
+
+        when:
+        service.probeVideo(file)
+
+        then:
+        def e = thrown(IllegalStateException)
+        e.message == 'ffprobe could not be found'
+
+        where:
+        _   |   path
+        _   |   null
+        _   |   'path' + File.separator + 'to' + File.separator + 'nowhere'
+    }
 }
