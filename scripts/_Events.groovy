124ef2eeb8be92db09e67ee12fadf099819491bd
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-06-20 20:43:33 -0400

    Handle even where config does not exist in _Events

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,12 +1,17 @@
 import grails.util.Environment
 import org.apache.commons.io.FileUtils
 
+includeTargets << grailsScript("_GrailsPackage")
+
 eventTestPhasesEnd = {
 
     // TODO: Remove this once we set Jenkins up on a remote server
     // This is only necessary while Jenkins and I share the same temporary directory
     if(Environment.current == Environment.TEST) {
-        def path = grailsApp.config.reeltime.storage.output
+        if(!binding.hasProperty('config')) {
+            createConfig()
+        }
+        def path = config.reeltime.storage.output
         def outputDirectory = new File(path)
 
         println "Deleting storage output directory: $path"
