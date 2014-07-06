aed122ff00d27739b6056c011c67a2ca225e8232
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-07-05 23:24:04 -0400

    Temporarily log sent emails for SES

--- grails-app/services/in/reeltime/mail/aws/SimpleEmailMailService.groovy
+++ grails-app/services/in/reeltime/mail/aws/SimpleEmailMailService.groovy
@@ -8,5 +8,8 @@ class SimpleEmailMailService implements MailService {
     void sendMail(String to, String from, String subject, String body) {
         // TODO: Integrate with SES
         log.debug "Entering [${this.class.name}..."
+
+        // TODO: This should be modified to not log the body, which contains the confirmation code, in production!
+        log.info "Sending email from [$from] to [$to] with subject [$subject] and body [$body]"
     }
 }
