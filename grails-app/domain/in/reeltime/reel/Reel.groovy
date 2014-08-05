fae0eaac76c9a6ee2508de49bed40e1a05bb07c5
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-08-05 17:41:23 -0400

    Add name to Reel model

--- grails-app/domain/in/reeltime/reel/Reel.groovy
+++ grails-app/domain/in/reeltime/reel/Reel.groovy
@@ -2,9 +2,12 @@ package in.reeltime.reel
 
 class Reel {
 
+    String name
+
     static hasOne = [audience: Audience]
 
     static constraints = {
+        name nullable: false, blank: false, size: 5..25
         audience unique: true
     }
 }
--- test/unit/in/reeltime/reel/ReelSpec.groovy
+++ test/unit/in/reeltime/reel/ReelSpec.groovy
@@ -2,9 +2,18 @@ package in.reeltime.reel
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 @TestFor(Reel)
 class ReelSpec extends Specification {
+
+    void "a reel with no audience is impossible"() {
+        when:
+        def reel = new Reel()
+
+        then:
+        !reel.validate(['audience'])
+    }
 
     void "a reel must have one audience"() {
         given:
@@ -14,6 +23,25 @@ class ReelSpec extends Specification {
         def reel = new Reel(audience: audience)
 
         then:
-        reel.validate()
+        reel.validate(['audience'])
+    }
+
+    @Unroll
+    void "name [#name] is valid [#valid]"() {
+        when:
+        def reel = new Reel(name: name)
+
+        then:
+        reel.validate(['name']) == valid
+
+        where:
+        name        |   valid
+        null        |   false
+        ''          |   false
+        'a'         |   false
+        'a' * 4     |   false
+        'a' * 5     |   true
+        'a' * 25    |   true
+        'a' * 26    |   false
     }
 }
