8835a1196c1cf089707d36258c80a740c6c5db18
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-08-31 15:22:09 -0400

    Remove video and schedule resource removal

--- grails-app/services/in/reeltime/video/VideoRemovalService.groovy
+++ grails-app/services/in/reeltime/video/VideoRemovalService.groovy
@@ -4,16 +4,33 @@ class VideoRemovalService {
 
     def reelVideoManagementService
     def resourceRemovalService
-
-    /*
-        1. Remove video from all reels
-        2. Collect all uris for playlists and segments
-        3. Create removal targets for uris
-        4. Delete video instance
-    */
+    def pathGenerationService
 
     void removeVideo(Video video) {
+        def videoId = video.id
 
+        log.info "Removing video [$videoId] from all reels"
         reelVideoManagementService.removeVideoFromAllReels(video)
+
+        log.info "Scheduling removal of master video for video [$videoId]"
+        def masterVideoBase = pathGenerationService.inputBase as String
+        resourceRemovalService.scheduleForRemoval(masterVideoBase, video.masterPath)
+
+        def playlistAndSegmentBase = pathGenerationService.outputBase
+
+        log.info "Scheduling removal of playlists for video [$videoId]"
+        video.playlistUris.each { playlistUri ->
+            resourceRemovalService.scheduleForRemoval(playlistAndSegmentBase, playlistUri.uri)
+        }
+
+        log.info "Scheduling removal of video segments for video [$videoId]"
+        video.playlists.each { playlist ->
+            playlist.segments.each { segment ->
+                resourceRemovalService.scheduleForRemoval(playlistAndSegmentBase, segment.uri)
+            }
+        }
+
+        log.info "Deleting video [$videoId]"
+        video.delete()
     }
 }
---
+++ test/integration/in/reeltime/video/VideoRemovalServiceIntegrationSpec.groovy
@@ -0,0 +1,91 @@
+package in.reeltime.video
+
+import grails.test.spock.IntegrationSpec
+import in.reeltime.playlist.Playlist
+import in.reeltime.playlist.PlaylistType
+import in.reeltime.playlist.Segment
+import in.reeltime.reel.Reel
+import in.reeltime.user.User
+import in.reeltime.maintenance.ResourceRemovalTarget
+import test.helper.UserFactory
+
+class VideoRemovalServiceIntegrationSpec extends IntegrationSpec {
+
+    def videoRemovalService
+    def pathGenerationService
+
+    User creator
+    Reel reel
+
+    String inputBase
+    String outputBase
+
+    void setup() {
+        creator = UserFactory.createTestUser()
+        reel = creator.reels[0]
+
+        inputBase = pathGenerationService.inputBase
+        outputBase = pathGenerationService.outputBase
+    }
+
+    void "remove video successfully and schedule resources for removal"() {
+        given:
+        def playlist = new Playlist()
+        playlist.addToSegments(segmentId: 1, uri: 'seg1.ts', duration: '1.0')
+        playlist.addToSegments(segmentId: 2, uri: 'seg2.ts', duration: '1.0')
+
+        assert playlist.segments.size() == 2
+
+        def video = new Video(
+                creator: creator,
+                title: 'some video',
+                masterPath: 'something.mp4',
+                reels: [reel]
+        )
+
+        video.addToPlaylists(playlist)
+
+        video.addToPlaylistUris(type: PlaylistType.Variant, uri: 'variant.m3u8')
+        video.addToPlaylistUris(type: PlaylistType.Media, uri: 'media.m3u8')
+
+        video.save(validate: false)
+
+        and:
+        [playlist, reel, video].each {
+            println "Asserting id for [$it]"
+            assert it?.id > 0
+        }
+        and:
+        def videoId = video.id
+        def playlistId = playlist.id
+
+        def segment1Id = playlist.segments[0].id
+        def segment2Id = playlist.segments[1].id
+
+        def reelId = reel.id
+
+        when:
+        videoRemovalService.removeVideo(video)
+
+        then:
+        Video.findById(videoId) == null
+        Playlist.findById(playlistId) == null
+
+        Segment.findById(segment1Id) == null
+        Segment.findById(segment2Id) == null
+
+        and:
+        def retrievedReel = Reel.findById(reelId)
+        retrievedReel != null
+        !retrievedReel.videos.contains(video)
+
+        and:
+        ResourceRemovalTarget.findByBaseAndRelative(inputBase, 'something.mp4') != null
+
+        ResourceRemovalTarget.findByBaseAndRelative(outputBase, 'seg1.ts') != null
+        ResourceRemovalTarget.findByBaseAndRelative(outputBase, 'seg2.ts') != null
+
+        ResourceRemovalTarget.findByBaseAndRelative(outputBase, 'variant.m3u8') != null
+        ResourceRemovalTarget.findByBaseAndRelative(outputBase, 'media.m3u8') != null
+    }
+}
