feeef1f29501764dfcc6569be63a5eda7e4c58cf
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-09-28 12:10:21 -0400

    Add UserFollowingController to follow and unfollow user

---
+++ grails-app/controllers/in/reeltime/user/UserFollowingController.groovy
@@ -0,0 +1,40 @@
+package in.reeltime.user
+
+import in.reeltime.common.AbstractController
+import in.reeltime.exceptions.UserNotFoundException
+
+import static javax.servlet.http.HttpServletResponse.*
+
+class UserFollowingController extends AbstractController {
+
+    def userService
+    def userFollowingService
+
+    def followUser(String username) {
+        handleSingleParamRequest(username, 'following.username.required') {
+            def currentUser = userService.currentUser
+            def userToFollow = userService.loadUser(username)
+
+            userFollowingService.startFollowingUser(currentUser, userToFollow)
+            render(status: SC_CREATED)
+        }
+    }
+
+    def unfollowUser(String username) {
+        handleSingleParamRequest(username, 'following.username.required') {
+            def currentUser = userService.currentUser
+            def userToUnfollow = userService.loadUser(username)
+
+            userFollowingService.stopFollowingUser(currentUser, userToUnfollow)
+            render(status: SC_OK)
+        }
+    }
+
+    def handleUserNotFoundException(UserNotFoundException e) {
+        exceptionErrorMessageResponse(e, 'following.unknown.username', SC_BAD_REQUEST)
+    }
+
+    def handleIllegalArgumentException(IllegalArgumentException e) {
+        exceptionErrorMessageResponse(e, 'following.invalid.request', SC_BAD_REQUEST)
+    }
+}
---
+++ test/unit/in/reeltime/user/UserFollowingControllerSpec.groovy
@@ -0,0 +1,131 @@
+package in.reeltime.user
+
+import grails.test.mixin.TestFor
+import in.reeltime.common.AbstractControllerSpec
+import in.reeltime.exceptions.UserNotFoundException
+import in.reeltime.message.LocalizedMessageService
+import spock.lang.Unroll
+
+@TestFor(UserFollowingController)
+class UserFollowingControllerSpec extends AbstractControllerSpec {
+
+    UserService userService
+    UserFollowingService userFollowingService
+
+    LocalizedMessageService localizedMessageService
+
+    User follower
+    User followee
+
+    def setup() {
+        userService = Mock(UserService)
+        userFollowingService = Mock(UserFollowingService)
+        localizedMessageService = Mock(LocalizedMessageService)
+
+        controller.userService = userService
+        controller.userFollowingService = userFollowingService
+        controller.localizedMessageService = localizedMessageService
+
+        follower = new User(username: 'follower')
+        followee = new User(username: 'followee')
+    }
+
+    void "current user begins following a user"() {
+        given:
+        params.username = 'followee'
+
+        when:
+        controller.followUser()
+
+        then:
+        assertStatusCodeOnlyResponse(response, 201)
+
+        and:
+        1 * userService.currentUser >> follower
+        1 * userService.loadUser('followee') >> followee
+
+        and:
+        1 * userFollowingService.startFollowingUser(follower, followee)
+    }
+
+    void "current user stops following a user"() {
+        given:
+        params.username = 'followee'
+
+        when:
+        controller.unfollowUser()
+
+        then:
+        assertStatusCodeOnlyResponse(response, 200)
+
+        and:
+        1 * userService.currentUser >> follower
+        1 * userService.loadUser('followee') >> followee
+
+        and:
+        1 * userFollowingService.stopFollowingUser(follower, followee)
+    }
+
+    @Unroll
+    void "username param is omitted for action [#actionName]"() {
+        when:
+        controller."$actionName"()
+
+        then:
+        assertErrorMessageResponse(response, 400, TEST_MESSAGE)
+
+        and:
+        1 * localizedMessageService.getMessage('following.username.required', request.locale) >> TEST_MESSAGE
+
+        where:
+        _   |   actionName
+        _   |   'followUser'
+        _   |   'unfollowUser'
+    }
+
+    @Unroll
+    void "user to follow cannot be found for action [#actionName]"() {
+        given:
+        params.username = 'nobody'
+
+        when:
+        controller."$actionName"()
+
+        then:
+        assertErrorMessageResponse(response, 400, TEST_MESSAGE)
+
+        and:
+        1 * userService.loadUser('nobody') >> { throw new UserNotFoundException('TEST') }
+        1 * localizedMessageService.getMessage('following.unknown.username', request.locale) >> TEST_MESSAGE
+
+        where:
+        _   |   actionName
+        _   |   'followUser'
+        _   |   'unfollowUser'
+    }
+
+    @Unroll
+    void "fail to execute following method [#methodName] for action [#actionName]"() {
+        given:
+        params.username = 'followee'
+
+        when:
+        controller."$actionName"()
+
+        then:
+        assertErrorMessageResponse(response, 400, TEST_MESSAGE)
+
+        and:
+        1 * userService.currentUser >> follower
+        1 * userService.loadUser('followee') >> followee
+
+        and:
+        1 * userFollowingService."$methodName"(follower, followee) >> { throw new IllegalArgumentException('TEST') }
+        1 * localizedMessageService.getMessage('following.invalid.request', request.locale) >> TEST_MESSAGE
+
+        where:
+        actionName      |   methodName
+        'followUser'    |   'startFollowingUser'
+        'unfollowUser'  |   'stopFollowingUser'
+    }
+}
